5cec5eb5bc8f488355e52b167a85d0dda95da844
Shawn McCabe <smccabe@acromediainc.com>
2020-04-23 16:54:42 -0700

    Added delete of previous phar to build

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 cp vendor/drupal/coder/coder_sniffer/Drupal/ vendor/squizlabs/php_codesniffer/src/Standards/ -r
 cp vendor/drupal/coder/coder_sniffer/DrupalPractice/ vendor/squizlabs/php_codesniffer/src/Standards/ -r
+rm phpdebt.phar
 ./vendor/bin/phar-composer build . 
